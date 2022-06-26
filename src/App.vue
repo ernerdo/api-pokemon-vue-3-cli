af05736d2c1c0f2b42fffb02c997c401122ba90d
Ernesto Herrera <jesh40@hotmail.es>
2022-06-26 09:36:58 -0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+  <PokedexApp></PokedexApp>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import HelloWorld from "./components/HelloWorld.vue";
+import PokedexApp from "./components/PokedexApp.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    // HelloWorld,
+    PokedexApp,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/PokedexForm.vue
@@ -0,0 +1,36 @@
+<template>
+  <form class="pokemon-form" on:submit="{handleSubmit}">
+    <input
+      class="pokemon-input"
+      type="text"
+      name="pokemon"
+      v-model="pokemonName"
+      placeholder="Busca tu pokemon"
+      autocomplete="off"
+    />
+    <input type="submit" class="pokemon-btn" value="" />
+  </form>
+</template>
+
+<script>
+export default {
+  name: "PokedexForm",
+
+  data() {
+    return {
+      pokemonName: "",
+    };
+  },
+
+  methods: {
+    search(e) {
+      e.preventDefault();
+      const pokemonName = this.pokemonName;
+      const newPokemonId = window.isNaN(parseInt(pokemonName))
+        ? pokemonName.toLowerCase()
+        : pokemonName;
+      this.$emit("submit", newPokemonId);
+    },
+  },
+};
+</script>
